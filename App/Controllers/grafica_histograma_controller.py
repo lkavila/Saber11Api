1d4650a963467f94d088c7195a9b7633399c420b
ton10q1 <arlintonjose.ap@gmail.com>
2021-11-11 17:13:26 -0500

    distri general

--- App/Controllers/grafica_histograma_controller.py
+++ App/Controllers/grafica_histograma_controller.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify, make_response
 
-from App.Domain.Analysis.diagrama_histograma import distribucionDeDatos
+from App.Domain.Analysis.diagrama_histograma import distribucionDeDatos, distribucionacumulada
 
 graficasHistograma = Blueprint('graficas_histograma', __name__)
 
@@ -28,5 +28,31 @@ def histogramamun():
 def histogramacol():
     data = request.args
     result = distribucionDeDatos(int(data.get('periodo','')),data.get('tpuntuacion'),data.get('departamento')
+                                 ,data.get('municipio'),data.get('colegio'))
+    return make_response(jsonify(result), 200)
+
+@graficasHistograma.route("/graficas/histograma-de-resultados-acumulados")
+def histogramaacumulado():
+    data = request.args
+    result = distribucionacumulada(int(data.get('periodo','')),data.get('tpuntuacion'),None,None,None)
+    return make_response(jsonify(result), 200)
+
+@graficasHistograma.route("/graficas/histograma-de-resultados-departamento-acumulados")
+def histogramadepacumulado():
+    data = request.args
+    result = distribucionacumulada(int(data.get('periodo','')),data.get('tpuntuacion'),data.get('departamento'),None,None)
+    return make_response(jsonify(result), 200)
+
+@graficasHistograma.route("/graficas/histograma-de-resultados-municipio-acumulados")
+def histogramamunacumulado():
+    data = request.args
+    result = distribucionacumulada(int(data.get('periodo','')),data.get('tpuntuacion'),data.get('departamento')
+                                 ,data.get('municipio'),None)
+    return make_response(jsonify(result), 200)
+
+@graficasHistograma.route("/graficas/histograma-de-resultados-colegio-acumulados")
+def histogramacolacumulado():
+    data = request.args
+    result = distribucionacumulada(int(data.get('periodo','')),data.get('tpuntuacion'),data.get('departamento')
                                  ,data.get('municipio'),data.get('colegio'))
     return make_response(jsonify(result), 200)
--- App/Domain/Analysis/diagrama_histograma.py
+++ App/Domain/Analysis/diagrama_histograma.py
@@ -8,15 +8,35 @@ def distribucionDeDatos(periodo,tpuntaje,departamento,municipio,colegio):
     bins= newbins(tpuntaje)
     names=newnames(tpuntaje)
     result.append({'ejex': names})
-    df=newdf(periodo,tpuntaje,departamento,municipio,colegio)
-    df[tpuntaje]=pd.cut(df[tpuntaje],bins,labels=names)
-    df = df.groupby(tpuntaje).size()
+    df=newdf(periodo,tpuntaje,departamento,municipio,colegio,bins,names)
+    total = 0
     for i in names:
-        r.append(int(df[i]))
+        j=int(df[i])
+        r.append(j)
+        total = total + j
 
     result.append({'ejey': r})
+    result.append({'Total de datos': total})
     return result
+def distribucionacumulada(calendario,tpuntaje,departamento,municipio,colegio):
+    result=[]
+    r = []
+    bins = newbins(tpuntaje)
+    names = newnames(tpuntaje)
+    result.append({'ejex': names})
+    df1 = newdf((2017*10)+calendario, tpuntaje, departamento, municipio, colegio,bins,names)
+    df2 = newdf((2018*10)+calendario, tpuntaje, departamento, municipio, colegio,bins,names)
+    df3 = newdf((2019*10)+calendario, tpuntaje, departamento, municipio, colegio,bins,names)
+    df4 = newdf((2020*10)+calendario, tpuntaje, departamento, municipio, colegio,bins,names)
+    total=0
+    for i in names:
+        j=int(df1[i]+df2[i]+df3[i]+df4[i])
+        r.append(j)
+        total=total+j
 
+    result.append({'ejey': r})
+    result.append({'Total de datos': total})
+    return result
 def newbins(tpuntaje):
     if (tpuntaje == 'PUNT_GLOBAL'):
         bins = [0, 50, 100, 150, 200, 250, 300, 350, 400, 450, 500]
@@ -32,7 +52,7 @@ def newnames(tpuntaje):
 
     return names
 
-def newdf(periodo,tpuntaje,departamento,municipio,colegio):
+def newdf(periodo,tpuntaje,departamento,municipio,colegio,bins,names):
     df = get_dataframe_for_year(periodo)
     df = mantener_columnas(df,[tpuntaje,'COLE_DEPTO_UBICACION','COLE_MCPIO_UBICACION','COLE_NOMBRE_ESTABLECIMIENTO'])
     if(departamento!=None):
@@ -41,4 +61,8 @@ def newdf(periodo,tpuntaje,departamento,municipio,colegio):
             df = df[df['COLE_MCPIO_UBICACION'] == municipio.upper()]
             if (colegio != None):
                 df = df[df['COLE_NOMBRE_ESTABLECIMIENTO'] == colegio.upper()]
+
+
+    df[tpuntaje]=pd.cut(df[tpuntaje],bins,labels=names)
+    df = df.groupby(tpuntaje).size()
     return df
