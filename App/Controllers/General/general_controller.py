15f63d5ca49a45830471d566fdefc24205808c25
lkavila <lkavila@uninorte.edu.co>
2021-11-11 16:38:26 -0500

    se agrego funcionalidad de porcentaje de desempeño a colegios, datos de los registros de colegios, probabilidad normal general y para colegios

--- App/Controllers/General/general_controller.py
+++ App/Controllers/General/general_controller.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify, make_response
-from App.Domain.Analisis.datos_generales import obtener_departamentos_y_municipios_colegios, total_registros_niveles_desempeno_por_calendario
+from App.Domain.Analisis.datos_generales import obtener_departamentos_y_municipios_colegios,\
+    total_registros_niveles_desempeno_por_calendario, probabilidad_puntaje
 
 generalData = Blueprint('general_data', __name__)
 
@@ -15,4 +16,20 @@ def obtener_departamentos_y_municipios():
 def obtener_promedios_y_desempenos():
     data = request.args
     result = total_registros_niveles_desempeno_por_calendario((data.get('calendario','')))
+    return make_response(jsonify(result), 200)
+
+
+@generalData.route('/datos-generales/probabilidad-puntaje')
+def probabilidad_de_puntaje():
+    data = request.args
+    result = {
+        "respuesta": probabilidad_puntaje(
+            int(data.get('periodo', '')),
+            data.get('puntaje', ''),
+            int(data.get('limite_inf', '')),
+            int(data.get('limite_sup', '')),
+            data.get('departamento'),
+            data.get('municipio'),
+           )
+    }
     return make_response(jsonify(result), 200)
